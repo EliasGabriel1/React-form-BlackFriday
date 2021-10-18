7a5b1b9b794f6777ba258d4f4e203c4c63b959b4
EliasGabriel1 <elias.147682@hotmail.com>
2021-10-17 22:57:56 -0300

    atualização 1.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const src = "https://eliasgabriel1.github.io/portifa-comunity/content/img/perfil
 function App() {
   return (
     <Main>
-      <img width="100" src={src}/>
+      <img width="100" src={src} alt="logo"/>
       <h1> Black Friday !</h1>
       <h2> Fique ligado(a) na única promoção que faço no ano! <br/> MOMENTO MAIS ESPERADO DO ANO!</h2>
 <FormNew/>
